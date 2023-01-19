733f4e128380d977d0f05dba425ede6088cba4d4
Menekam Rudy <menekamrudy@gmail.com>
2023-01-19 09:08:07 +0100

    Fix issue with book not adding

--- js/index.js
+++ js/index.js
@@ -1,31 +1,42 @@
-const btn = document.querySelector('.addBtn');
-const bookContainer = document.querySelector('.bookContainer');
-const books = JSON.parse(localStorage.getItem('books')) || [];
-function onAddElement(e) {
-  e.preventDefault();
-  const title = document.getElementById('insertTitle').value;
-  const author = document.getElementById('insertAuthor').value;
-  bookContainer.innerHTML += `
-        <section>   
-        <p>${title}</p>
-            <p>${author}</p>
-            <button class="deleteBtn" >Remove</button>
-            <hr>
-            </section>
-        `;
-  books.push({ title, author });
-  localStorage.setItem('books', JSON.stringify(books));
-  console.log(books);
-}
-
-function onDeleteRow(e) {
-  if (!e.target.classList.contains('deleteBtn')) {
-    return;
+class Library {
+  constructor() {
+    this.book = JSON.parse(localStorage.getItem('books')) || [];
   }
 
-  const btn = e.target;
-  btn.closest('section').remove();
-}
+  addBook = () => {
+    const title = document.getElementById('insertTitle').value;
+    const author = document.getElementById('insertAuthor').value;
+    if (title.trim() === '' || author.trim() === '') return;
+    const id = Date.now().toString();
+    this.book.push({ title, author, id });
 
-btn.addEventListener('click', onAddElement);
-bookContainer.addEventListener('click', onDeleteRow);
+    localStorage.setItem('books', JSON.stringify(this.book));
+    window.location.reload();
+  };
+
+  removeBook = (id) => {
+    const filteredBooks = this.book.filter((b) => b.id !== id);
+    localStorage.setItem('books', JSON.stringify(filteredBooks));
+    window.location.reload();
+  };
+}
+const bookContainer = document.querySelector('.bookContainer');
+const g = new Library();
+g.book.forEach((b) => {
+  bookContainer.innerHTML += `
+      <div class="innerContainer">
+      <p>${b.title}</p>
+      <p>${b.author}</p>
+        <button class="deleteBtn" id="${b.id}">Remove</button>
+      </div>
+    `;
+});
+
+bookContainer.addEventListener('click', (e) => {
+  const clickedBtn = e.target.closest('.deleteBtn');
+  if (!clickedBtn) return;
+  const idToRemove = clickedBtn.id;
+  g.removeBook(idToRemove);
+});
+
+document.querySelector('.addBtn').addEventListener('click', g.addBook);
